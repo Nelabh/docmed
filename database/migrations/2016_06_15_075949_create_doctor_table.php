1809f46f2b0abba99748f2f4018ba38f6081b2b3
Sushumna Khandelwal <sush.94kh@gmail.com>
2016-06-15 15:43:22 +0530

    Migrations craeted

---
+++ database/migrations/2016_06_15_075949_create_doctor_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDoctorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->increments('doc_id');
+            $table->string('doc_name');
+            $table->string('email');
+            $table->integer('mobile')->length(10);
+            $table->string('gender');
+            $table->integer('age');
+            $table->string('speciality');
+            $table->string('qualification');
+            $table->string('hospital_affiliation');
+            $table->integer('years_of_exp');
+            $table->string('mci');
+            $table->string('current_position');
+            $table->string('address');
+            $table->string('city');
+            $table->integer('pincode');
+            $table->string('state');
+            $table->string('country');
+            $table->tinyInteger('verified');
+            $table->string('upload_photograph');
+
+            $table->rememberToken();
+            $table->timestamps();
+            $table->foreign('email')->references('email')->on('users')->onDelete('cascade');
+        });
+        //
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('doctors');
+    }
+}

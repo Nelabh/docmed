86e6d7394cfe66536344a91716d0f6814ae10a19
Nelabh Kotiya <nelabhkotiya@gmail.com>
2016-06-17 19:24:30 +0530

    Minors

--- resources/views/leftnavigation.blade.php
+++ resources/views/leftnavigation.blade.php
@@ -1,33 +1,47 @@
 <aside id="left-panel">
 
-			<!-- User info -->
-			<div class="login-info">
-				<span> <!-- User image size is adjusted inside CSS, it should stay as it --> 
-					
-					<a href="javascript:void(0);" id="show-shortcut" data-action="toggleShortcut">
-						<img src="img/avatars/sunny.png" alt="me" class="online" /> 
-						<span>
-							User Name
-						</span>
-					</a> 
-					
-				</span>
-			</div>
-			<!-- end user info -->
+	<!-- User info -->
+	<div class="login-info">
+		<span> <!-- User image size is adjusted inside CSS, it should stay as it --> 
 
-			<!-- NAVIGATION : This navigation is also responsive-->
-			<nav>
+			<a href="javascript:void(0);" id="show-shortcut" data-action="toggleShortcut">
+				<img src="img/avatars/sunny.png" alt="me" class="online" /> 
+				<span>
+					User Name
+				</span>
+			</a> 
+
+		</span>
+	</div>
+	<!-- end user info -->
+
+	<!-- NAVIGATION : This navigation is also responsive-->
+	<nav>
 				<!-- 
 				NOTE: Notice the gaps after each icon usage <i></i>..
 				Please note that these links work a bit different than
 				traditional href="" links. See documentation for details.
-				-->
+			-->
+			<ul>
+				@if(Request::path() == '/')
+				<li class="active">
+					<a href="{{URL::route('dashboard')}}" title="Dashboard"><i class="fa fa-lg fa-fw fa-home"></i> <span class="menu-item-parent">Dashboard</span></a>			
+				</li>
+				@else
+				<li>
+					<a href="{{URL::route('dashboard')}}" title="Dashboard"><i class="fa fa-lg fa-fw fa-home"></i> <span class="menu-item-parent">Dashboard</span></a>
+				</li>
+				@endif
 
-				<ul>
-					<li class="active">
-						<a href="#" title="Dashboard"><i class="fa fa-lg fa-fw fa-home"></i> <span class="menu-item-parent">Dashboard</span></a>
-							
-					</li>
+				@if(Request::path() == 'history')
+				<li class="active">
+					<a href="{{URL::route('dashboard')}}" title="History"><i class="fa fa-lg fa-fw fa-home"></i> <span class="menu-item-parent">History</span></a>			
+				</li>
+				@else
+				<li>
+					<a href="{{URL::route('dashboard')}}" title="History"><i class="fa fa-lg fa-fw fa-home"></i> <span class="menu-item-parent">History</span></a>
+				</li>
+				@endif
 <!-- 					<li class="top-menu-invisible">
 						<a href="#"><i class="fa fa-lg fa-fw fa-cube txt-color-blue"></i> <span class="menu-item-parent">SmartAdmin Intel</span></a>
 						<ul>
@@ -42,7 +56,7 @@
 							</li>
 						</ul>
 					</li> -->
-		
+
 				</ul>
 			</nav>
 			

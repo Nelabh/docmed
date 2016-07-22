58737849ef20d2b2f1330ec3f71790cb0b543209
Sushumna Khandelwal <sush.94kh@gmail.com>
2016-07-22 19:30:14 +0530

    .

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -13,25 +13,55 @@ use App\Http\Controllers\MedVendController;
 use App\Http\Controllers\DoctorController;
 use App\Doctor;
 use App\Speciality;
+use App\HealthStatus;
+use App\Connection;
+use App\Patient;
 use Redirect;
 use Illuminate\Support\Facades\Input;
 
 class DoctorController extends BaseController
 {
-    use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
-public function __construct()
-{
-    $this->middleware('auth');
-}
+	use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
+	public function __construct()
+	{
+		$this->middleware('auth');
+	}
 	public static function dashboard(){
 
 		switch (Auth::user()->level) {
 			case '1':
-			 $doc=Doctor::where('email',Auth::user()->email)->first();
-			 if($doc->verified==0)
-			 	return View::make('notverified');
-			 else
-				return View::make('doctor_dashboard');
+			$doc=Doctor::where('email',Auth::user()->email)->first();
+			$con = Connection::where('doctor_email',Auth::user()->email)->get();
+			if(count($con))
+			{
+				
+				foreach($con as $c)
+				{
+					$patient = Patient::where('email',$c->patient_email)->first();
+					$health = HealthStatus::where('id',$c->health_record)->first();
+					$c->problem = $health->problem;
+					$c->statement = $health->statement;
+					$c->name
+			
+
+				}
+				
+			}
+			else
+			{
+				$patient=[];
+				$health=[];
+
+
+			}
+
+
+			if($doc->verified!=0)
+				return View::make('doctor_dashboard',compact('patient')); 	
+			
+			else
+				return View::make('notverified');
+
 			break;
 			case '2':
 			return UserController::dashboard();
@@ -45,59 +75,59 @@ public function __construct()
 			return Redirect::route('home');
 			break;
 */		}
+			
+		}
+
+		public function profile_doctor(){
+			if(Auth::user()->level==1)
+			{
+				$speciality=Speciality::all();
+
+
+				$doc = Doctor::where('email',Auth::user()->email)->first();
+				return View::make('profile_doctor',compact('doc','speciality'));
+
+			}
+		}
+		public function edit()
+		{
+			if(Auth::user()->level==1)
+			{
+				$data=Input::all();
+
+				$doc = Doctor::where('email',Auth::user()->email)->first();
+				$doc->doc_name=$data['doc_name'];
+				$doc->mobile=$data['mobile'];
+				$doc->age=$data['age'];
+				$doc->speciality=$data['speciality'];
+				$doc->country=$data['country'];
+				$doc->state=$data['state'];
+				$doc->city=$data['city'];
+				$doc->pincode=$data['pincode'];
+				$doc->current_position=$data['current_position'];
+				$doc->qualification=$data['qualification'];
+				$doc->address=$data['address'];
+				$doc->hospital_affiliation=$data['hospital_affiliation'];
+				$doc->years_of_exp=$data['years_of_exp'];
+				$doc->mci=$data['mci'];
+
+				$doc->save();
+				
+				return view::make('doctor_dashboard');
+
+
+
+
+
+
+
+
+
+
+
+			}
+		}
+
 		
 	}
-
-	public function profile_doctor(){
-		if(Auth::user()->level==1)
-		{
-		$speciality=Speciality::all();
-
-
-			$doc = Doctor::where('email',Auth::user()->email)->first();
-		return View::make('profile_doctor',compact('doc','speciality'));
-
-		}
-	}
-	public function edit()
-	{
-		if(Auth::user()->level==1)
-		{
-			$data=Input::all();
-
-			$doc = Doctor::where('email',Auth::user()->email)->first();
-			$doc->doc_name=$data['doc_name'];
-			$doc->mobile=$data['mobile'];
-			$doc->age=$data['age'];
-			$doc->speciality=$data['speciality'];
-			$doc->country=$data['country'];
-			$doc->state=$data['state'];
-			$doc->city=$data['city'];
-			$doc->pincode=$data['pincode'];
-			$doc->current_position=$data['current_position'];
-			$doc->qualification=$data['qualification'];
-			$doc->address=$data['address'];
-			$doc->hospital_affiliation=$data['hospital_affiliation'];
-			$doc->years_of_exp=$data['years_of_exp'];
-			$doc->mci=$data['mci'];
-
-			$doc->save();
-			
-			return view::make('doctor_dashboard');
-
-
-
-
-
-
-
-
-
-
-
-		}
-	}
-
-	
-}
 
--- resources/views/doctor_dashboard.blade.php
+++ resources/views/doctor_dashboard.blade.php
@@ -89,34 +89,7 @@
 			<!-- MAIN CONTENT -->
 			<div id="content">
 
-				<div class="row">
-					<div class="col-xs-12 col-sm-7 col-md-7 col-lg-4">
-						<h1 class="page-title txt-color-blueDark"><i class="fa-fw fa fa-home"></i> Dashboard </h1>
-					</div>
-					<div class="col-xs-12 col-sm-5 col-md-5 col-lg-8">
-						<ul id="sparks" class="">
-							<li class="sparks-info">
-								<h5> My Income <span class="txt-color-blue">$47,171</span></h5>
-								<div class="sparkline txt-color-blue hidden-mobile hidden-md hidden-sm">
-									1300, 1877, 2500, 2577, 2000, 2100, 3000, 2700, 3631, 2471, 2700, 3631, 2471
-								</div>
-							</li>
-							<!-- <li class="sparks-info">
-								<h5> Site Traffic <span class="txt-color-purple"><i class="fa fa-arrow-circle-up"></i>&nbsp;45%</span></h5>
-								<div class="sparkline txt-color-purple hidden-mobile hidden-md hidden-sm">
-									110,150,300,130,400,240,220,310,220,300, 270, 210
-								</div>
-							</li>
-							<li class="sparks-info">
-								<h5> Site Orders <span class="txt-color-greenDark"><i class="fa fa-shopping-cart"></i>&nbsp;2447</span></h5>
-								<div class="sparkline txt-color-greenDark hidden-mobile hidden-md hidden-sm">
-									110,150,300,130,400,240,220,310,220,300, 270, 210
-								</div>
-							</li> -->
-						</ul>
-					</div>
-				</div>
-				<!-- widget grid -->
+				
 				<section id="widget-grid" class="">
 
 		<div class="row">
@@ -126,19 +99,7 @@
 				
 							<!-- Widget ID (each widget will need unique ID)-->
 							<div class="jarviswidget jarviswidget-color-darken" id="wid-id-0" data-widget-editbutton="false">
-								<!-- widget options:
-								usage: <div class="jarviswidget" id="wid-id-0" data-widget-editbutton="false">
-				
-								data-widget-colorbutton="false"
-								data-widget-editbutton="false"
-								data-widget-togglebutton="false"
-								data-widget-deletebutton="false"
-								data-widget-fullscreenbutton="false"
-								data-widget-custombutton="false"
-								data-widget-collapsed="true"
-								data-widget-sortable="false"
-				
-								-->
+								
 								<header>
 									<span class="widget-icon"> <i class="fa fa-table"></i> </span>
 									<h2>List of Patients Request </h2>
@@ -163,66 +124,82 @@
 												<tr>
 													<th data-hide="phone">Sno.</th>
 													<th data-class="expand"><i class="fa fa-fw fa-user text-muted hidden-md hidden-sm hidden-xs"></i>Patient Name</th>
-													<th>Symptoms</th>
-													<th data-hide="phone,tablet">Location</th>
+													<th data-hide="phone,tablet">Problem</th>
+													<th data-hide="phone,tablet">Statement</th>
+													<th data-hide="phone,tablet">City</th>
+													<th data-hide="phone,tablet">Phone No.</th>
+
 													<th data-hide="phone,tablet">Status</th>
-
-
 													<th data-hide="phone,tablet">Accept</th>
 												</tr>
 											</thead>
 											<tbody>
-												<tr>
-													<td>1</td>
-													<td>Jennifer</td>
-													<td>1-342-463-8341</td>
-													<td>Et Rutrum Non Associates</td>
-													<td>Pending</td>
+												@if(count($patient))
+												@foreach($patient as $p)
+													@if($p->status==0)
+
+												<tr bgcolor="#FF0000">
+													<?php $id=0; ?>
+													<td><?php  $id++; ?></td>
+													<td>$p->name</td>
+													<td>$p->problem</td>
+													<td>$p->statement</td>
+													<td>$p->city</td>
+													<td>$p->mobile</td>
+													<td>PENDING</td>
 
 													<td><a href="javascript:void(0);" class="btn btn-success btn-circle"><i class="fa fa-check"></i></a></td>
 
 												</tr>
-												<tr>
-													<td>2</td>
-													<td>Clark</td>
-													<td>1-516-859-1120</td>
-													<td>Nam Ac Inc.</td>
-													<td>Urgent</td>
+												@elseif($p->status==1)
+												<tr bgcolor="#FFFFFF">
 
-													<td><a href="javascript:void(0);" class="btn btn-success btn-circle"><i class="fa fa-check"></i></a></td>
-
-
-												</tr>
-												<tr>
-													<td>3</td>
-													<td>Brendan</td>
-													<td>1-724-406-2487</td>
-													<td>Enim Commodo Limited</td>
-													<td>Pending</td>
-
-													<td><a href="javascript:void(0);" class="btn btn-success btn-circle"><i class="fa fa-check"></i></a></td>
-													
-												</tr>
-												<tr>
-													<td>4</td>
-													<td>Warren</td>
-													<td>1-412-485-9725</td>
-													<td>Odio Etiam Institute</td>
-													<td>Urgent</td>
+													<?php $id=1; ?>
+													<td><?php  $id++; ?></td>
+													<td>$p->name</td>
+													<td>$p->problem</td>
+													<td>$p->statement</td>
+													<td>$p->city</td>
+													<td>$p->mobile</td>
+													<td>ONGOING</td>
 
 													<td><a href="javascript:void(0);" class="btn btn-success btn-circle"><i class="fa fa-check"></i></a></td>
 
 												</tr>
-												<tr>
-													<td>5</td>
-													<td>Rajah</td>
-													<td>1-849-642-8777</td>
-													<td>Neque Ltd</td>
-													<td>Urgent</td>
+												@else
+												<tr bgcolor="#FFF000">
+
+													<?php $id=1; ?>
+													<td><?php  $id++; ?></td>
+													<td>$p->name</td>
+													<td>$p->problem</td>
+													<td>$p->statement</td>
+													<td>$p->city</td>
+													<td>$p->mobile</td>
+													<td>URGENT</td>
 
 													<td><a href="javascript:void(0);" class="btn btn-success btn-circle"><i class="fa fa-check"></i></a></td>
 
 												</tr>
+												@endif
+												@endforeach
+												@else
+												<tr>
+												<td>-</td>
+												<td>-</td>
+
+												<td>-</td>
+
+												<td>-</td>
+
+												<td>-</td>
+												<td>-</td>
+												<td>-</td>
+												<td>-</td>
+
+												</tr>
+												@endif
+												
 												
 											</tbody>
 										</table>
